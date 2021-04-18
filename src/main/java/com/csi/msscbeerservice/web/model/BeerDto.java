c0cf90fc47d8211048b76f489f8d5d58b911f76b
prajul-shinde <prajulshinde@gmail.com>
2021-04-18 12:10:37 +0530

    adding jackson config for BeerDto. closes #13

--- src/main/java/com/csi/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/com/csi/msscbeerservice/web/model/BeerDto.java
@@ -1,5 +1,6 @@
 package com.csi.msscbeerservice.web.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -21,8 +22,12 @@ public class BeerDto {
     private UUID id;
     @Null
     private Integer version;
+
+    @JsonFormat(pattern = "yyyy:MM:dd'T':HH:mm:ssZ",shape = JsonFormat.Shape.STRING)
     @Null
     private OffsetDateTime createdDate;
+
+    @JsonFormat(pattern = "yyyy:MM:dd'T':HH:mm:ssZ",shape = JsonFormat.Shape.STRING)
     @Null
     private OffsetDateTime lastModifiedDate;
 
@@ -33,6 +38,8 @@ public class BeerDto {
     @Positive
     @NotNull
     private Long upc;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     @Positive
     @NotNull
     private BigDecimal price;
---
+++ target/generated-sources/annotations/com/csi/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -0,0 +1,35 @@
+package com.csi.msscbeerservice.web.mappers;
+
+import com.csi.msscbeerservice.domain.Beer;
+import com.csi.msscbeerservice.web.model.BeerDto;
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2021-04-15T15:06:50+0530",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 16 (Oracle Corporation)"
+)
+public class BeerMapperImpl implements BeerMapper {
+
+    @Override
+    public BeerDto beerToBeerDto(Beer beer) {
+        if ( beer == null ) {
+            return null;
+        }
+
+        BeerDto beerDto = new BeerDto();
+
+        return beerDto;
+    }
+
+    @Override
+    public Beer beerDtoToBeer(BeerDto beerDto) {
+        if ( beerDto == null ) {
+            return null;
+        }
+
+        Beer beer = new Beer();
+
+        return beer;
+    }
+}
